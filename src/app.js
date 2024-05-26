04ae86165cd2da56bb6b606f89d285d9938a541b
BagusITK <11211021@student.itk.ac.id>
2024-05-26 18:04:13 +0800

    inisialisasi project dan testing

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(routes);
+app.get('/', (req, res) => {
+  res.send('testing API')
+})
 
 app.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
